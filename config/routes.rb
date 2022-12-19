1d0e1bd41ef53611c74d582bb473ace150087bfd
Gohar Vardanyan <gohsamvelyan@gmail.com>
2022-12-19 23:29:47 +0100

    watch list avc devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   devise_for :users
+#   root to: "pages#home"
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   root to: "lists#index"
+#   resources :bookmarks, only: [:new, :create]
+#   resources :reviews, only: :create
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+# resources :bookmarks, only: :destroy
+root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
